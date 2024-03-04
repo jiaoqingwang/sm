537d9c328234ea282d54b9c879538b7d795f3a83
jiaoqingwang <543447709@qq.com>
2024-03-04 22:13:05 +0800

    Changes

--- src/main/java/com/example/demo2/HelloServlet.java
+++ src/main/java/com/example/demo2/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello Jiaoqingwang!";
+        message = "Hello World!jiaoqingwang";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
